22dc58708704c7a416de30ea712e22be7d10a353
Jesse <jessesmspringer@gmail.com>
2019-01-27 09:03:52 -0500

    added learn more button and page

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return render_template('home.html')
+def home():
+    return render_template("home.html")
+
+@app.route("/learnmore", methods=["GET"])
+def learn_more():
+    return render_template("learnmore.html")
 
 if __name__ == '__main__':
-    hello()
+    home()
